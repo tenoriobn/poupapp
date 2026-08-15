a9ac11f18ed8fd50db9d7e780dffe93b882aeedb
Bruno Tenorio <tenoriobn@gmail.com>
2026-08-15 17:35:43 -0300

    feat: :sparkles: Módulo 5: Dinamizando os modais

--- src/app/area-financeira/area-financeira.component.ts
+++ src/app/area-financeira/area-financeira.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { SaldoComponent } from './saldo/saldo.component';
 import { TransacoesComponent } from './transacoes/transacoes.component';
 import { ContasComponent } from './contas/contas.component';
@@ -14,7 +14,7 @@ import { Transacao, TipoTransacao } from './compartilhados/transacao.model';
 export class AreaFinanceiraComponent {
   saldo = -30;
 
-  transacoes: Transacao[] = [
+  transacoes = signal<Transacao[]>([
     {
       id: '5',
       nome: '',
@@ -55,9 +55,9 @@ export class AreaFinanceiraComponent {
       data: new Date('2024-10-01T00:00'),
       conta: 'Anybank',
     },
-  ];
+  ]);
 
-  contas: Conta[] = [
+  contas = signal<Conta[]>([
     {
       nome: 'Anybank',
       saldo: 1000,
@@ -70,5 +70,13 @@ export class AreaFinanceiraComponent {
       nome: 'Switch Bank',
       saldo: 0,
     },
-  ];
+  ]);
+
+  processarTransacao(transacao: Transacao) {
+    this.transacoes.update((transacoes) => [transacao, ...transacoes]);
+  }
+
+  adicionarConta(conta: Conta) {
+    this.contas.update((contas) => [...contas, conta]);
+  }
 }
--- src/app/area-financeira/transacoes/transacoes.component.ts
+++ src/app/area-financeira/transacoes/transacoes.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { TransacaoComponent } from './transacao/transacao.component';
 import { Transacao } from '../compartilhados/transacao.model';
 import { BotaoComponent } from '../../compartilhados/botao/botao.component';
@@ -17,4 +17,6 @@ import { BotaoAdicionarTransacaoComponent } from './botao-adicionar-transacao/bo
 })
 export class TransacoesComponent {
   transacoes = input.required<Transacao[]>();
+
+  transacaoCriada = output<Transacao>();
 }
