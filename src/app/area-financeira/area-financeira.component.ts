9eddc5684288fc801d041841a7715b184fc5fb5a
Bruno Tenorio <tenoriobn@gmail.com>
2026-08-16 09:18:01 -0300

    feat: :sparkles: Módulo 06: Realizando integrações entre os dados

--- src/app/area-financeira/area-financeira.component.ts
+++ src/app/area-financeira/area-financeira.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { SaldoComponent } from './saldo/saldo.component';
 import { TransacoesComponent } from './transacoes/transacoes.component';
 import { ContasComponent } from './contas/contas.component';
@@ -12,71 +12,51 @@ import { Transacao, TipoTransacao } from './compartilhados/transacao.model';
   styleUrl: './area-financeira.component.css',
 })
 export class AreaFinanceiraComponent {
-  saldo = -30;
+  transacoes = signal<Transacao[]>([]);
 
-  transacoes = signal<Transacao[]>([
-    {
-      id: '5',
-      nome: '',
-      tipo: TipoTransacao.SAQUE,
-      valor: 200,
-      data: new Date('2025-02-20T00:00'),
-      conta: 'Switch Bank',
-    },
-    {
-      id: '4',
-      nome: 'Almoço',
-      tipo: TipoTransacao.SAQUE,
-      valor: 40,
-      data: new Date('2025-01-15T00:00'),
-      conta: 'Bytebank',
-    },
-    {
-      id: '3',
-      nome: '',
-      tipo: TipoTransacao.DEPOSITO,
-      valor: 400,
-      data: new Date('2025-01-10T00:00'),
-      conta: 'Bytebank',
-    },
-    {
-      id: '2',
-      nome: 'Freela (2ª parte)',
-      tipo: TipoTransacao.DEPOSITO,
-      valor: 200,
-      data: new Date('2024-10-01T00:00'),
-      conta: 'Anybank',
-    },
-    {
-      id: '1',
-      nome: 'Freela (1ª parte)',
-      tipo: TipoTransacao.DEPOSITO,
-      valor: 100,
-      data: new Date('2024-10-01T00:00'),
-      conta: 'Anybank',
-    },
-  ]);
+  contasComSaldoInicial = signal<Conta[]>([]);
 
-  contas = signal<Conta[]>([
-    {
-      nome: 'Anybank',
-      saldo: 1000,
-    },
-    {
-      nome: 'Bytebank',
-      saldo: 0,
-    },
-    {
-      nome: 'Switch Bank',
-      saldo: 0,
-    },
-  ]);
+  contas = computed(() => {
+    return this.contasComSaldoInicial().map((conta) => {
+      const saldoAtualizado = this.calculaSaldoAtualizado(conta);
+
+      return { ...conta, saldo: saldoAtualizado };
+    });
+  });
+
+  saldo = computed(() => {
+    return this.contas().reduce((acc, conta) => {
+      return acc + conta.saldo;
+    }, 0);
+  });
+
+  calculaSaldoAtualizado(contaInicial: Conta) {
+    const transacoesDaConta = this.transacoes().filter((transacao) => {
+      return transacao.conta === contaInicial.nome;
+    });
+
+    const novoSaldo = transacoesDaConta.reduce((acc, transacao) => {
+      switch (transacao.tipo) {
+        case TipoTransacao.DEPOSITO:
+          return acc + transacao.valor;
+
+        case TipoTransacao.SAQUE:
+          return acc - transacao.valor;
+
+        default:
+          transacao.tipo satisfies never;
+          throw new Error('Tipo de transação  não identificado.');
+      }
+    }, contaInicial.saldo);
+
+    return novoSaldo;
+  }
 
   processarTransacao(transacao: Transacao) {
     this.transacoes.update((transacoes) => [transacao, ...transacoes]);
   }
 
   adicionarConta(conta: Conta) {
-    this.contas.update((contas) => [...contas, conta]);
+    this.contasComSaldoInicial.update((contas) => [...contas, conta]);
   }
 }
